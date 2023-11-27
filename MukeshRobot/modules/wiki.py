adc427bbb5a12c53ffe268261bf591ccc812f8b1
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-28 03:53:05 +0530

    Update wiki.py

--- MukeshRobot/modules/wiki.py
+++ MukeshRobot/modules/wiki.py
@@ -59,6 +59,6 @@ dispatcher.add_handler(WIKI_HANDLER)
 __help__ = """
 » /wiki (text) *:* sᴇᴀʀᴄʜs ᴀʙᴏᴜᴛ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴡɪᴋɪᴘᴇᴅɪᴀ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥](https://t.me/ABOUT_SHIVANSHOP)............☆
 """
-__mod_name__ = "⚡Wɪᴋɪ⚡"
+__mod_name__ = "✯Wɪᴋɪ✯"
