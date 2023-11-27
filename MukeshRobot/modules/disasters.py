9efd59b442aa5e4c1c3d1b1a07a67d7f2c8b19e8
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-27 23:59:48 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -630,7 +630,7 @@ __help__ = f"""
 `⚠️ ʀᴇᴀᴅ ғʀᴏᴍ ᴛᴏᴘ`
 ᴠɪsɪᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](f"https://t.me{SUPPORT_CHAT}") ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](f"https://t.me/the_vip_boy")............☆
+☆............𝙱𝚈 » [𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥](f"https://t.me/ITSZ_SHIVANSH")............☆
 """
 
 SUDO_HANDLER = CommandHandler("addsudo", addsudo)
@@ -663,7 +663,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "♨️ᴅᴇᴠꜱ♨️"
+__mod_name__ = "✯ᴅᴇᴠꜱ✯"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
