70f0e794675d802d939bd7da747d2da939419bb3
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-27 23:32:03 +0530

    Update aifunctions.py

--- MukeshRobot/modules/aifunctions.py
+++ MukeshRobot/modules/aifunctions.py
@@ -28,7 +28,7 @@ async def hmm(event):
     lodu = response.json()
     result = lodu["message"]["text"]
     if "no no" in result:
-        pro = "I am fairly found and I was made by @itz_legend_coder."
+        pro = "I am fairly found and I was made by @ITSZ_SHIVANSH."
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)
