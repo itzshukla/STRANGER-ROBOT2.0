c388894fe04a46219f11313130ed4b7ca4377bc8
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-27 23:43:38 +0530

    Update blacklist.py

--- MukeshRobot/modules/blacklist.py
+++ MukeshRobot/modules/blacklist.py
@@ -449,7 +449,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "♨️Bʟᴀᴄᴋʟɪsᴛ♨️"
+__mod_name__ = "✯Bʟᴀᴄᴋʟɪsᴛ✯"
 
 __help__ = """
 
@@ -464,7 +464,7 @@ Admin only:
  ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
  ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥](https://t.me/ITSZ_SHIVANSH)............☆
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
