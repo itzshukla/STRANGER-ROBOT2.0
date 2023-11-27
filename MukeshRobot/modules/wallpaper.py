ad4de4dce7434c7605abe4b58d4dd05b550a23f8
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-28 03:50:22 +0530

    Update wallpaper.py

--- MukeshRobot/modules/wallpaper.py
+++ MukeshRobot/modules/wallpaper.py
@@ -52,7 +52,7 @@ async def wall(client, message):
 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
 ❄ **ᴅᴏᴡɴʟᴏᴀᴅ :** `{murl}`
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥](https://t.me/ABOUT_SHIVANSHOP)............☆
 """
         await m.delete()
         await client.send_photo(
