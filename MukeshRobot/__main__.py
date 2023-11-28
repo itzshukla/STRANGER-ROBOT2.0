768531d2cd30a1f3761636cbaccbd439d21a5b35
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-28 09:47:49 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -1108,7 +1108,7 @@ def main():
         try:
             dispatcher.bot.sendAnimation(
                 f"@{SUPPORT_CHAT}",
-                animation="https://te.legra.ph/file/7ca788e000b2722d0bd5c.jpg",
+                animation="https://te.legra.ph/file/e7ac7415ab4395d3edc05.mp4",
                 caption=f"""
 {dispatcher.bot.first_name}
 
